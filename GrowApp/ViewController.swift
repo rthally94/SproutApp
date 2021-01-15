9bbcde559d110222ec5d86949180442c36ce4b53
Ryan Thally <rthally94@gmail.com>
2021-01-15 10:01:35 -0500

    Initial Commit

---
+++ GrowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GrowApp
+//
+//  Created by Ryan Thally on 1/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
