28bb111d68c89da3263a40fd4ec1920d8f97a382
Ryan Thally <rthally94@gmail.com>
2021-04-29 12:10:27 -0400

    Implemented ability to lookup all tasks
    Added View Controller to show all tasks and their respective due dates.

--- GrowApp/Features/Up Next/TasksProvider.swift
+++ GrowApp/Features/Up Next/TasksProvider.swift
@@ -5,4 +5,57 @@
 //  Created by Ryan Thally on 4/29/21.
 //
 
-import Foundation
+import CoreData
+import UIKit
+
+struct UpNextSection<Item>: Hashable where Item: Hashable {
+    var headerTitle: String
+    var items: [Item]
+}
+
+class TasksProvider: NSObject {
+    typealias Item = NSManagedObjectID
+    typealias Section = String
+
+    let moc: NSManagedObjectContext
+    fileprivate let fetchedResultsController: NSFetchedResultsController<GHTask>
+
+    @Published var snapshot: NSDiffableDataSourceSnapshot<Section, Item>?
+
+    init(managedObjectContext: NSManagedObjectContext) {
+        self.moc = managedObjectContext
+
+        let request: NSFetchRequest<GHTask> = GHTask.fetchRequest()
+        let sortByNextTaskDate = NSSortDescriptor(keyPath: \GHTask.nextCareDate, ascending: true)
+        let sortByTaskName = NSSortDescriptor(keyPath: \GHTask.taskType?.name, ascending: true)
+        let sortByPlantName = NSSortDescriptor(keyPath: \GHTask.plant?.name, ascending: true)
+        request.sortDescriptors = [sortByNextTaskDate, sortByTaskName, sortByPlantName]
+
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: moc, sectionNameKeyPath: #keyPath(GHTask.nextCareDate), cacheName: nil)
+
+        super.init()
+
+        fetchedResultsController.delegate = self
+        try! fetchedResultsController.performFetch()
+    }
+
+    func object(at indexPath: IndexPath) -> GHTask {
+        return fetchedResultsController.object(at: indexPath)
+    }
+}
+
+extension TasksProvider: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChangeContentWith snapshot: NSDiffableDataSourceSnapshotReference) {
+        var newSnapshot = snapshot as NSDiffableDataSourceSnapshot<Section, Item>
+        let idsToReload = newSnapshot.itemIdentifiers.filter { identifier in
+            guard let oldIndex = self.snapshot?.indexOfItem(identifier), let newIndex = newSnapshot.indexOfItem(identifier), oldIndex == newIndex else { return false }
+            guard (try? controller.managedObjectContext.existingObject(with: identifier))?.isUpdated == true else { return false }
+            return true
+        }
+
+        newSnapshot.reloadItems(idsToReload)
+        self.snapshot = newSnapshot
+    }
+}
+
+
--- GrowApp/Features/Up Next/UpNextViewController.swift
+++ GrowApp/Features/Up Next/UpNextViewController.swift
@@ -5,25 +5,111 @@
 //  Created by Ryan Thally on 4/29/21.
 //
 
+import Combine
+import CoreData
 import UIKit
 
 class UpNextViewController: UIViewController {
+    // MARK: - Properties
+    typealias Section = TasksProvider.Section
+    typealias Item = TasksProvider.Item
+
+    let viewContext: NSManagedObjectContext
+    let tasksProvider: TasksProvider
+
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    var cancellables: Set<AnyCancellable> = []
+
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: makeLayout())
+        cv.backgroundColor = .clear
+        cv.delegate = self
+        return cv
+    }()
+
+    // MARK: - Initializers
+    init(viewContext: NSManagedObjectContext) {
+        self.viewContext = viewContext
+        self.tasksProvider = TasksProvider(managedObjectContext: viewContext)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View Life Cycle
+    override func loadView() {
+        super.loadView()
+        setupViews()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = makeDataSource()
+        tasksProvider.$snapshot
+            .sink {[weak self] snapshot in
+                if let snapshot = snapshot {
+                    self?.dataSource.apply(snapshot)
+                }
+            }
+            .store(in: &cancellables)
 
-        // Do any additional setup after loading the view.
+        title = "Up Next"
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupViews() {
+        view.addSubview(collectionView)
+        collectionView.frame = view.bounds
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
     }
-    */
+}
+
+private extension UpNextViewController {
+    func makeLayout() -> UICollectionViewLayout {
+        var config = UICollectionLayoutListConfiguration(appearance: .sidebarPlain)
+        config.headerMode = .supplementary
+        return UICollectionViewCompositionalLayout.list(using: config)
+    }
+
+    func makeTaskCellRegistration() -> UICollectionView.CellRegistration<TaskCalendarListCell, Item> {
+        UICollectionView.CellRegistration<TaskCalendarListCell, Item> {[weak self] cell, indexPath, item in
+            guard let task = self?.tasksProvider.object(at: indexPath) else { return }
+            cell.updateWithTask(task)
+        }
+    }
+
+    func createHeaderRegistration() -> UICollectionView.SupplementaryRegistration<UICollectionViewListCell> {
+        return UICollectionView.SupplementaryRegistration<UICollectionViewListCell>(elementKind: UICollectionView.elementKindSectionHeader) {[unowned self] cell, elementKind, indexPath in
+            guard elementKind == UICollectionView.elementKindSectionHeader else { return }
+            let section = self.dataSource.snapshot().sectionIdentifiers[indexPath.section]
+            var configuration = UIListContentConfiguration.largeGroupedHeader()
+            configuration.text = section
+//            configuration.secondaryText = itemCount == 1 ? "\(itemCount) task" : "\(itemCount) tasks"
+            cell.contentConfiguration = configuration
+        }
+    }
+
+    func makeDataSource() -> UICollectionViewDiffableDataSource<Section, Item> {
+        let taskCellRegistration = makeTaskCellRegistration()
+        let dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView) { collectionView, indexPath, item in
+            collectionView.dequeueConfiguredReusableCell(using: taskCellRegistration, for: indexPath, item: item)
+        }
+
+        let taskHeader = createHeaderRegistration()
+        dataSource.supplementaryViewProvider = { collectionView, elementKind, indexPath in
+            switch elementKind {
+                case UICollectionView.elementKindSectionHeader:
+                    return collectionView.dequeueConfiguredReusableSupplementary(using: taskHeader, for: indexPath)
+                default:
+                    return nil
+            }
+        }
+
+        return dataSource
+    }
+}
+
+extension UpNextViewController: UICollectionViewDelegate {
 
 }
